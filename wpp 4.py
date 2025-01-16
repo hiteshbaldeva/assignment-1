5983d2d35bf72811d2e35ecf01af5d2e69934fdf
hiteshbaldeva <hsmaheshwari2005@gmail.com>
2025-01-16 14:13:18 +0530

    Delete wpp 4.py

--- wpp 4.py
+++
@@ -1,9 +0,0 @@
-# swap two variable without using third variable
-
-a= int(input("enter first number"))
-b= int(input("enter second number"))
-a=a+b
-b=a-b
-a=a-b
-print("after swap")
-print(f"a = {a} and b = {b}")
